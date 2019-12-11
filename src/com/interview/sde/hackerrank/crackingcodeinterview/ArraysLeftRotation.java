4f799fc436c8c20b7ffec97ad32a5427b11d1433
v.camarggo <vinicamarggo@hotmail.com>
2019-12-11 21:30:00 +0000

    Java exercises from HackerRank

---
+++ src/com/interview/sde/hackerrank/crackingcodeinterview/ArraysLeftRotation.java
@@ -0,0 +1,65 @@
+package com.interview.sde.hackerrank.crackingcodeinterview;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+//https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
+public class ArraysLeftRotation {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        int[] rotated = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            if (a.length - 1 >= d + i) {
+                rotated[i] = a[i + d];
+            } else {
+                rotated[i] = a[i + d - a.length];
+            }
+
+        }
+
+        return rotated;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
