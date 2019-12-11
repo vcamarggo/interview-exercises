5cd06a396a3a50eee0295277ab9341a70aef8ed5
v.camarggo <vinicamarggo@hotmail.com>
2019-12-11 20:49:19 +0000

    Tree exercises from HackerRank

---
+++ src/com/interview/sde/hackerrank/algorithm/trees/HeightofaBinaryTree.java
@@ -0,0 +1,28 @@
+package com.interview.sde.hackerrank.algorithm.trees;
+
+//https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem
+public class HeightofaBinaryTree {
+
+    class Node {
+        int data;
+        Node left;
+        Node right;
+    }
+
+    public static int height(Node root) {
+        if (root == null) {
+            return -1;
+        } else {
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+            if (leftHeight > rightHeight) {
+                return leftHeight + 1;
+            } else {
+                return rightHeight + 1;
+            }
+        }
+
+    }
+
+
+}
