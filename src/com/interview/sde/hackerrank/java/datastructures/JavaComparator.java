6c278476b52320731eea2a708b89b58d0ebd4ca4
v.camarggo <vinicamarggo@hotmail.com>
2019-12-17 15:12:04 +0000

    Java challenges

---
+++ src/com/interview/sde/hackerrank/java/datastructures/JavaComparator.java
@@ -0,0 +1,11 @@
+package com.interview.sde.hackerrank.java.datastructures;
+//https://www.hackerrank.com/challenges/java-comparator/problem
+/*class Checker implements Comparator<Player> {
+    // complete this method
+    public int compare(Player a, Player b) {
+        if(a.score == b.score){
+            return a.name.compareTo(b.name);
+        }
+        return a.score> b.score?-1:1;
+    }
+}*/
---
+++ src/com/interview/sde/hackerrank/java/datastructures/JavaGenerics.java
@@ -0,0 +1,42 @@
+package com.interview.sde.hackerrank.java.datastructures;
+
+import java.lang.reflect.Method;
+
+//https://www.hackerrank.com/challenges/java-generics/problem
+public class JavaGenerics {
+
+    static class Printer {
+        //With Object class
+  /* void printArray(Object[] array){
+       for(Object o : array){
+           System.out.println(o);
+       }
+   }*/
+        <T> void printArray(T[] array) {
+            for (T o : array) {
+                System.out.println(o);
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if (name.equals("printArray"))
+                count++;
+        }
+
+        if (count > 1) System.out.println("Method overloading is not allowed!");
+
+    }
+}
