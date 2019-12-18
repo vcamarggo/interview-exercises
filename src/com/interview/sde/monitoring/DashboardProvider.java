7385a029909abdd08bc0059ae3facb9f4423f952
v.camarggo <vinicamarggo@hotmail.com>
2019-12-18 19:40:46 +0000

    Java challenges

---
+++ src/com/interview/sde/monitoring/DashboardProvider.java
@@ -0,0 +1,42 @@
+package com.interview.sde.monitoring;
+
+import java.util.*;
+
+public class DashboardProvider {
+    private static HashMap<Long, DataPoint> dataset = new HashMap<>();
+
+    public static void main(String[] args) {
+        Random randomGenerator = new Random();
+
+        receiveData(System.currentTimeMillis(), List.of(randomGenerator.nextDouble()));
+
+        long t2 = System.currentTimeMillis();
+        receiveData(t2,
+                List.of(randomGenerator.nextDouble(),
+                        randomGenerator.nextDouble(),
+                        randomGenerator.nextDouble(),
+                        randomGenerator.nextDouble()));
+
+        printStatistics(t2);
+
+    }
+
+    static void receiveData(Long timestamp, List<Double> data) {
+        DataPoint d1 = new DataPoint();
+        dataset.putIfAbsent(timestamp, d1);
+        dataset.computeIfPresent(timestamp, (aLong, dataPoint) -> {
+            dataPoint.insertData(data);
+            return dataPoint;
+        });
+    }
+
+    static void printStatistics(Long timestamp) {
+        dataset.computeIfPresent(timestamp, (aLong, dataPoint) -> {
+            dataPoint.printMin();
+            dataPoint.printMax();
+            dataPoint.printAverage();
+            return dataPoint;
+        });
+    }
+
+}
---
+++ src/com/interview/sde/monitoring/DataPoint.java
@@ -0,0 +1,43 @@
+package com.interview.sde.monitoring;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DataPoint {
+    private List<Double> data;
+    private BigDecimal sumForAverage;
+    private double min = Double.MAX_VALUE;
+    private double max = Double.MIN_VALUE;
+
+    public DataPoint() {
+        this.data = new LinkedList<>();
+        this.sumForAverage = new BigDecimal(0);
+    }
+
+    public void insertData(Double data) {
+        insertData(List.of(data));
+    }
+
+    public void insertData(List<Double> dataList) {
+        for (Double d : dataList) {
+            this.min = Math.min(min, d);
+            this.max = Math.max(max, d);
+            sumForAverage = sumForAverage.add(new BigDecimal(d));
+            data.add(d);
+        }
+    }
+
+    public void printMin() {
+        System.out.println("Min: " + min);
+    }
+
+    public void printMax() {
+        System.out.println("Max: " + max);
+    }
+
+    public void printAverage() {
+        System.out.println("Avg: " + sumForAverage.divide(new BigDecimal(data.size()), RoundingMode.UNNECESSARY));
+    }
+}
