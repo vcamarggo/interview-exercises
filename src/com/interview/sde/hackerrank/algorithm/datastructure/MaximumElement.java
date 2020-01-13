3f60bd77de6887947d77866e52e49d09c1352f7f
v.camarggo <vinicamarggo@hotmail.com>
2020-01-13 19:00:44 +0000

    Heap exercises

---
+++ src/com/interview/sde/hackerrank/algorithm/datastructure/MaximumElement.java
@@ -0,0 +1,30 @@
+package com.interview.sde.hackerrank.algorithm.datastructure;
+
+import java.util.*;
+
+public class MaximumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int queries = Integer.parseInt(scanner.nextLine());
+
+        Stack<Integer> storageQueue = new Stack<>();
+        Stack<Integer> maxQueue = new Stack<>();
+
+        for (int i = 0; i < queries; i++) {
+            String[] query = scanner.nextLine().split(" ");
+            switch (query[0]) {
+                case "1":
+                    int numberToInsert = Integer.parseInt(query[1]);
+                    storageQueue.push(numberToInsert);
+                    maxQueue.push(maxQueue.isEmpty() ? numberToInsert : Math.max(maxQueue.peek(), numberToInsert));
+                    continue;
+                case "2":
+                    storageQueue.pop();
+                    maxQueue.pop();
+                    continue;
+                case "3":
+                    System.out.println(maxQueue.peek());
+            }
+        }
+    }
+}
