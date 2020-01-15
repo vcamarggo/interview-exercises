fc61e58d86ada42654c0acb66fa22f73ece62741
v.camarggo <vinicamarggo@hotmail.com>
2020-01-15 17:51:06 +0000

    Dequeue Exercise

---
+++ src/com/interview/sde/hackerrank/algorithm/linkedlist/PrintReverse.java
@@ -0,0 +1,99 @@
+package com.interview.sde.hackerrank.algorithm.linkedlist;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+//https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list-in-reverse/problem
+public class PrintReverse {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.print(sep);
+            }
+        }
+    }
+
+    // Complete the reversePrint function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void reversePrint(SinglyLinkedListNode head) {
+        Stack<Integer> reversedList = new Stack<>();
+        while (head != null) {
+            reversedList.push(head.data);
+            head = head.next;
+        }
+        while (!reversedList.isEmpty()) {
+            System.out.println(reversedList.pop());
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int tests = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            reversePrint(llist.head);
+        }
+
+        scanner.close();
+    }
+}
