ea258077a87602d0e10f371b30cf7e24b67b581c
v.camarggo <vinicamarggo@hotmail.com>
2020-01-20 18:44:04 +0000

    warmup algorithms

---
+++ src/com/interview/sde/hackerrank/algorithm/strings/CaesarCipher.java
@@ -0,0 +1,53 @@
+package com.interview.sde.hackerrank.algorithm.strings;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+//https://www.hackerrank.com/challenges/caesar-cipher-1/problem
+public class CaesarCipher {
+
+    static String caesarCipher(String s, int k) {
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            int charAscCode = charArray[i];
+            int rotatedAscCode = charAscCode + k;
+            if (charAscCode >= 65 && charAscCode <= 90) {
+                if (charAscCode + k > 90) {
+                    rotatedAscCode = 64 + (charAscCode + k - 90);
+                }
+                charArray[i] = (char) rotatedAscCode;
+            } else if (charAscCode >= 97 && charAscCode <= 122) {
+                if (charAscCode + k > 122) {
+                    rotatedAscCode = 96 + (charAscCode + k - 122);
+                }
+                charArray[i] = (char) rotatedAscCode;
+            }
+        }
+        return new String(charArray);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int k = scanner.nextInt() % 26;
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String result = caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
--- src/com/interview/sde/hackerrank/crackingcodeinterview/RecursionFibonacciNumbers.java
+++ src/com/interview/sde/hackerrank/crackingcodeinterview/RecursionFibonacciNumbers.java
@@ -1,16 +1,36 @@
 package com.interview.sde.hackerrank.crackingcodeinterview;
 
 import java.util.Scanner;
+
 //https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
 public class RecursionFibonacciNumbers {
 
     static int[] memo = new int[31];
 
-    public static int fibonacci(int n) {
+    public static int fibonacci3(int n) {
+        for (int i = 2; i <= n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+        return memo[n];
+    }
+
+    public static int fibonacci2(int n) {
         if (memo[n] == 0 && n != 0) {
             memo[n] = fibonacci(n - 2) + fibonacci(n - 1);
         }
         return memo[n];
+    }
+
+    public static int fibonacci(int n) {
+        int n2 = 0;
+        int n1 = 1;
+        int result;
+        for (int i = 2; i <= n; i++) {
+            result = n1 + n2;
+            n2 = n1;
+            n1 = result;
+        }
+        return n1;
     }
 
 
@@ -23,3 +43,4 @@ public class RecursionFibonacciNumbers {
         System.out.println(fibonacci(n));
     }
 }
+
