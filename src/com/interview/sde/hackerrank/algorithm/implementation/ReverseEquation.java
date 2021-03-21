b9210646622ad29e2bf1c72efdcb18190edfe50c
Vinicius Camargo <cvinici@amazon.com>
2021-03-21 22:38:31 +0000

    Add leetcode exercises

---
+++ src/com/interview/sde/hackerrank/algorithm/implementation/ReverseEquation.java
@@ -0,0 +1,26 @@
+package com.interview.sde.hackerrank.algorithm.implementation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//https://leetcode.com/problems/find-positive-integer-solution-for-a-given-equation/
+public class ReverseEquation {
+    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+        List<List<Integer>> solution = new ArrayList<>();
+
+        for (int x = 1; x <= 1000; x++) {
+            for (int y = 1; y <= 1000; y++) {
+                if (customfunction.f(x, y) == z) {
+                    solution.add(Arrays.asList(x, y));
+                    break;
+                }
+            }
+        }
+        return solution;
+    }
+
+    private interface CustomFunction {
+        int f(int x, int y);
+    }
+}
