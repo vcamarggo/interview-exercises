42183e4588c1b0c9bb7fbbb940044e6c7ece427c
Vinicius Camargo <cvinici@amazon.com>
2021-03-24 21:17:16 +0000

    Permutation for subsets and ordering for matrix

---
+++ src/com/interview/sde/hackerrank/algorithm/arrays/GenerateAllSubsets.java
@@ -0,0 +1,33 @@
+package com.interview.sde.hackerrank.algorithm.arrays;
+
+import java.util.*;
+
+//https://leetcode.com/problems/subsets/
+public class GenerateAllSubsets {
+    public static void main(String[] args) {
+        System.out.println(subsets(new int[]{4, 1, 2, 3}));
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> solution = new ArrayList<>();
+
+        for (int number : nums) {
+            if (!solution.isEmpty()) {
+                int solutionSize = solution.size();
+                //For each new numbers, run to all existing solutions and and this new number to the existing solution
+                for (int i = 0; i < solutionSize; i++) {
+                    List<Integer> aggregator = new ArrayList<>(solution.get(i));
+                    aggregator.add(number);
+                    solution.add(aggregator);
+                }
+            }
+            //Add the new number a single solution
+            solution.add(new ArrayList<>(Collections.singletonList(number)));
+        }
+        //Add empty solution
+        solution.add(new ArrayList<>());
+        return solution;
+    }
+
+
+}
