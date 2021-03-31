2fd47643cf03ed2833e73d6d2a92106825851a4f
Vinicius Camargo <cvinici@amazon.com>
2021-03-31 20:45:19 +0100

    Array and String exercises

---
+++ src/com/interview/sde/hackerrank/algorithm/search/FindPeak.java
@@ -0,0 +1,31 @@
+package com.interview.sde.hackerrank.algorithm.search;
+
+//https://leetcode.com/problems/find-peak-element
+public class FindPeak {
+
+    public int findPeakElement(int[] nums) {
+        if(nums.length >= 2){
+            if(nums[nums.length-1] > nums[nums.length-2]){
+                return nums.length-1;
+            }
+            if(nums[0] > nums[1]){
+                return 0;
+            }
+        }
+
+        for(int i = 1; i < nums.length-1; i++){
+            if (nums[i-1] < nums[i] && nums[i] > nums[i+1]){
+                return i;
+            }
+        }
+
+        //For legibility, I'd remove the first if and move it to the next lines like, it makes the conditions simpler
+//        if(nums.length >= 2){
+//            if(nums[nums.length-1] > nums[nums.length-2]){
+//                return nums.length-1;
+//            }
+//        }
+
+        return 0;
+    }
+}
