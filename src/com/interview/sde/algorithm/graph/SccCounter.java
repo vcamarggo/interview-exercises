7b9af8815f0e30fe7b359e92da008655bf30797e
Vinicius Camargo <cvinici@amazon.com>
2021-06-09 19:18:58 +0100

    SCC Counter based on Udacity Gratuate Algorithms

--- src/com/interview/sde/algorithm/graph/SccCounter.java
+++ src/com/interview/sde/algorithm/graph/SccCounter.java
@@ -1,16 +1,19 @@
 package com.interview.sde.algorithm.graph;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
-public class TopologicalOrder {
+//To design this I opted to not pass parameters all around. It's clearly overusing static attributes
+public class SccCounter {
 
 
     static int postNumber = 1;
+    static int sccId = 1;
     static Set<Integer> visited = new HashSet<>();
     static boolean[][] edges;
+    static boolean[][] reverseEdges;
     static int[] postOrderNumber;
+    static int[] scc;
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -19,7 +22,9 @@ public class TopologicalOrder {
         int edgesNumber = Integer.parseInt(gameSize[1]);
 
         edges = new boolean[vertex + 1][vertex + 1];
+        reverseEdges = new boolean[vertex + 1][vertex + 1];
         postOrderNumber = new int[vertex + 1];
+        scc = new int[vertex + 1];
 
         //Start end ignored
         String[] startEnd = scanner.nextLine().split(" ");
@@ -33,40 +38,50 @@ public class TopologicalOrder {
             int node1Id = Integer.parseInt(edgeData[0]);
             int node2Id = Integer.parseInt(edgeData[1]);
             edges[node1Id][node2Id] = true;
+            reverseEdges[node2Id][node1Id] = true;
         }
 
-        topologicalOrder(edges);
+        sccCounter();
 
     }
 
-    private static void topologicalOrder(boolean[][] edges) {
+    private static void sccCounter() {
 
-        for (int node = 1; node < edges.length; node++) {
+        for (int node = 1; node < reverseEdges.length; node++) {
             if (!visited.contains(node)) {
-                dfs(node);
+                explore(node, reverseEdges);
             }
         }
 
-        // Fancy O(n) version
+        // Fancy O(n) version for ordering based on dfs post order number
         int[] topologicalOrder = new int[postOrderNumber.length * 2];
         for (int i = 1; i < postOrderNumber.length; i++) {
             topologicalOrder[postOrderNumber[i]] = i;
         }
 
+        visited.clear();
+
         for (int i = topologicalOrder.length - 1; i >= 0; i--) {
-            if (topologicalOrder[i] != 0) {
-                System.out.println(topologicalOrder[i]);
+            int node = topologicalOrder[i];
+            if (node != 0 && !visited.contains(node)) {
+                scc[node] = sccId++;
+                explore(node, edges);
             }
         }
+
+        //Ignore the initial 0, vertex starts at 1
+        System.out.println(Arrays.toString(scc));
+        System.out.println(Arrays.stream(scc).filter(i -> i != 0).distinct().count());
+
     }
 
-
-    private static void dfs(int node) {
+    private static void explore(int node, boolean[][] graph) {
         postNumber++;
         visited.add(node);
-        for (int destination = 1; destination < edges.length; destination++) {
-            if (edges[node][destination] && !visited.contains(destination)) {
-                dfs(destination);
+        scc[node] = sccId;
+        for (int destination = 1; destination < graph.length; destination++) {
+            if (graph[node][destination] && !visited.contains(destination)) {
+                explore(destination, graph);
             }
         }
         postOrderNumber[node] = postNumber++;
