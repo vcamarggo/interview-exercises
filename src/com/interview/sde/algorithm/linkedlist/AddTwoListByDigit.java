74777103375e4137106ec10a4bf44a04d12a7c82
Vinicius Camargo <cvinici@amazon.com>
2021-06-11 08:36:21 +0100

    Add two list by digits from 2n to n

--- src/com/interview/sde/algorithm/linkedlist/AddTwoListByDigit.java
+++ src/com/interview/sde/algorithm/linkedlist/AddTwoListByDigit.java
@@ -22,9 +22,12 @@ public class AddTwoListByDigit {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode l1Head = l1;
         ListNode l2Head = l2;
+
+        ListNode lastUsedNode = null;
+
         boolean carryOver = false;
-        int l1Size = 0;
-        int l2Size = 0;
+        int l1SizeDiff = 0;
+
         while (l1 != null || l2 != null) {
             int newVal = carryOver ? 1 : 0;
 
@@ -32,37 +35,34 @@ public class AddTwoListByDigit {
                 newVal += (l1.val + l2.val);
                 l1.val = newVal % 10;
                 l2.val = newVal % 10;
+                lastUsedNode = l1;
                 l1 = l1.next;
                 l2 = l2.next;
-                l1Size++;
-                l2Size++;
             } else if (l2 != null) {
                 newVal += l2.val;
                 l2.val = newVal % 10;
+                lastUsedNode = l2;
                 l2 = l2.next;
-                l2Size++;
+                l1SizeDiff--;
             } else {
                 //l1 is not null
                 newVal += l1.val;
                 l1.val = newVal % 10;
+                lastUsedNode = l1;
                 l1 = l1.next;
-                l1Size++;
+                l1SizeDiff++;
             }
 
             //mod 10 had overflow
             carryOver = newVal >= 10;
+
         }
 
         if (carryOver) {
-            ListNode head = l1Size > l2Size ? l1Head : l2Head;
-            int nodesToProcess = Math.max(l1Size, l2Size);
-            while (--nodesToProcess > 0) {
-                head = head.next;
-            }
-            head.next = new ListNode(1);
+            lastUsedNode.next = new ListNode(1);
         }
 
-        return l1Size > l2Size ? l1Head : l2Head;
+        return l1SizeDiff >= 0 ? l1Head : l2Head;
     }
 
     public static void main(String[] args) {
