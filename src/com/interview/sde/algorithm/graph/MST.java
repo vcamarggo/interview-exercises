bbb7488d14ba1ae4d76e1b564e4ca1aec30b63b3
Vinicius Camargo <cvinici@amazon.com>
2021-06-11 14:49:58 +0100

    MST Prim

--- src/com/interview/sde/algorithm/graph/MST.java
+++ src/com/interview/sde/algorithm/graph/MST.java
@@ -30,9 +30,11 @@ public class MST {
         }
 
         kruskal(edges);
+        System.out.println();
+        prim(edges);
     }
 
-    private static void kruskal(int[][] edges) {
+    private static void kruskal(final int[][] edges) {
         PriorityQueue<int[]> edgesToProcess = new PriorityQueue<>(Comparator.comparingInt(arr -> arr[2]));
         ArrayList<int[]> solution = new ArrayList<>();
         int[] parents = new int[edges.length];
@@ -58,9 +60,81 @@ public class MST {
             }
         }
 
+        System.out.println("Kruskal solution:");
         for (int[] edge : solution) {
             System.out.println(String.format("Edge %02d <-> %02d - weight %s", edge[0], edge[1], edge[2]));
         }
+    }
+
+    static class PrimNode {
+        public int parent;
+        int id;
+        int weight;
+
+        public PrimNode(int id, int weight, int parent) {
+            this.id = id;
+            this.weight = weight;
+            this.parent = parent;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PrimNode primNode = (PrimNode) o;
+            return id == primNode.id;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
+    }
+
+    private static void prim(final int[][] edges) {
+        //QuickAccess to node
+        Map<Integer, PrimNode> allNodes = new HashMap<>();
+        Map<Integer, PrimNode> solution = new HashMap();
+
+        boolean[] processed = new boolean[edges.length];
+
+        for (int i = 1; i < edges.length; i++) {
+            PrimNode primNode = new PrimNode(i, Integer.MAX_VALUE, i);
+            allNodes.put(primNode.id, primNode);
+        }
+        allNodes.get(1).weight -=1;
+
+        while (solution.size() != edges.length - 1) {
+
+            PrimNode node = getMin(allNodes, processed);
+            solution.put(node.id, node);
+
+            for (int i = 1; i < edges.length; i++) {
+                if (allNodes.get(i).weight > edges[node.id][i] && edges[node.id][i] != 0 && !solution.containsKey(i)) {
+                    allNodes.get(i).parent = node.id;
+                    allNodes.get(i).weight = edges[node.id][i];
+                }
+            }
+        }
+
+        System.out.println("Prim solution:");
+        for (int i = 2; i < edges.length; i++) {
+            System.out.println(String.format("Edge %02d <-> %02d - weight %s", solution.get(i).parent, i, edges[i][solution.get(i).parent]));
+        }
+
+    }
+
+    private static PrimNode getMin(Map<Integer, PrimNode> allNodes, boolean[] processed) {
+        int min = Integer.MAX_VALUE, min_index = -1;
+
+        for (int v = 1; v < processed.length; v++)
+            if (!processed[v] && allNodes.get(v).weight < min) {
+                min = allNodes.get(v).weight;
+                min_index = v;
+            }
+
+        processed[min_index] = true;
+        return allNodes.get(min_index);
     }
 
     public static void union(int[] parents, int a, int b) {
