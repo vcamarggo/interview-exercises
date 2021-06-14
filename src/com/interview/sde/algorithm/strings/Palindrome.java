a5365df4f8cd88170cb5adf459a3d7bc58a0e3ae
Vinícius Camargo <vinicamarggo@hotmail.com>
2021-06-14 15:39:47 +0100

    Create Palindrome.java

---
+++ src/com/interview/sde/algorithm/strings/Palindrome.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/valid-palindrome/
+class Palindrome {
+    public boolean isPalindrome(String s) {
+        s  = s.replaceAll(
+          "[^a-zA-Z0-9]", "").toLowerCase();
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--)
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+        return true;
+    }
+}
