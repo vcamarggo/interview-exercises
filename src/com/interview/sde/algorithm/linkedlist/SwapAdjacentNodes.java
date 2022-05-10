d34d9f1528e9f995807e8bd2d9b3e4a3d60f7789
vcamarggo <vinicamarggo@hotmail.com>
2022-05-10 08:40:52 +0100

    Reformat code

--- src/com/interview/sde/algorithm/linkedlist/SwapAdjacentNodes.java
+++ src/com/interview/sde/algorithm/linkedlist/SwapAdjacentNodes.java
@@ -2,18 +2,10 @@ package com.interview.sde.algorithm.linkedlist;
 
 //https://leetcode.com/problems/swap-nodes-in-pairs/
 public class SwapAdjacentNodes {
-    public static class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
-
     static ListNode swapPairs(ListNode head) {
         ListNode sentinel = new ListNode(Integer.MIN_VALUE, head);
         ListNode current = sentinel;
-        while (current.next != null && current.next.next != null){ //If there are swappable elements
+        while (current.next != null && current.next.next != null) { //If there are swappable elements
             ListNode leftSwap = current.next;
 
             ListNode nextLeftSwap = current.next.next.next;
@@ -29,5 +21,22 @@ public class SwapAdjacentNodes {
 
     public static void main(String[] args) {
         swapPairs(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))));
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
