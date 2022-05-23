59c5bd4610d51270382cd4e5224191115c7058eb
vcamarggo <vinicamarggo@hotmail.com>
2022-05-23 19:10:53 +0100

    Level tree

---
+++ src/com/interview/sde/algorithm/tree/BinaryTreeLowestCommonAncestor.java
@@ -0,0 +1,52 @@
+package com.interview.sde.algorithm.tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+public class BinaryTreeLowestCommonAncestor {
+    static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> pathToP = findPath(root, p);
+        List<TreeNode> pathToQ = findPath(root, q);
+        int minSizePath = Math.min(pathToP.size(), pathToQ.size());
+        for (int i = 0; i < minSizePath; i++) {
+            if (pathToP.get(i) != pathToQ.get(i)) {
+                return pathToP.get(i - 1);
+            }
+        }
+        return pathToP.get(minSizePath - 1);
+    }
+
+    static List<TreeNode> findPath(TreeNode root, TreeNode toFind) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        if (root == toFind) {
+            List<TreeNode> list = new ArrayList<>();
+            list.add(root);
+            return list;
+        }
+        List<TreeNode> solutionLeft = findPath(root.left, toFind);
+        if (!solutionLeft.isEmpty()) {
+            solutionLeft.add(0, root);
+            return solutionLeft;
+        }
+        List<TreeNode> solutionRight = findPath(root.right, toFind);
+        if (!solutionRight.isEmpty()) {
+            solutionRight.add(0, root);
+            return solutionRight;
+        }
+        return Collections.emptyList();
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/com/interview/sde/algorithm/tree/DeepestLeavesSum.java
@@ -0,0 +1,36 @@
+package com.interview.sde.algorithm.tree;
+
+//https://leetcode.com/problems/deepest-leaves-sum/
+public class DeepestLeavesSum {
+    public int deepestLeavesSum(TreeNode root) {
+        int deepestNode = height(root);
+        return deepestLeavesSum(root, 1, deepestNode);
+    }
+
+    int deepestLeavesSum(TreeNode root, int height, int maxHeight) {
+        return root == null ? 0 : root.left == null && root.right == null && height == maxHeight ? root.val : deepestLeavesSum(root.left, height + 1, maxHeight) + deepestLeavesSum(root.right, height + 1, maxHeight);
+    }
+
+    int height(TreeNode root) {
+        return root == null ? 0 : Math.max(height(root.left), height(root.right)) + 1;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
