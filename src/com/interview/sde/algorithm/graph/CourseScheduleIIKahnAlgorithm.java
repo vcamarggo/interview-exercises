8236df683891e2e5f2166eab10d9290487302196
vcamarggo <vinicamarggo@hotmail.com>
2022-05-30 19:36:40 +0100

    Topological sort

--- src/com/interview/sde/algorithm/graph/CourseScheduleIIKahnAlgorithm.java
+++ src/com/interview/sde/algorithm/graph/CourseScheduleIIKahnAlgorithm.java
@@ -3,27 +3,20 @@ package com.interview.sde.algorithm.graph;
 import java.util.*;
 
 //https://leetcode.com/problems/course-schedule-ii/
-public class CourseScheduleII {
+public class CourseScheduleIIKahnAlgorithm {
+
     public static void main(String[] args) {
         System.out.println(
-                Arrays.toString(findOrder(9,
+                Arrays.toString(findOrder(4,
                         new int[][]{
-                                new int[]{1, 4},
-                                new int[]{2, 1},
-                                new int[]{2, 3},
-                                new int[]{2, 5},
-                                new int[]{3, 6},
-                                new int[]{4, 5},
-                                new int[]{4, 7},
-                                new int[]{4, 8},
-                                new int[]{5, 7},
-                                new int[]{6, 7},
-                                new int[]{6, 8},
-                                new int[]{7, 8}
+                                new int[]{1, 0},
+                                new int[]{2, 0},
+                                new int[]{3, 1},
+                                new int[]{3, 2},
                         })));
     }
 
-    static int[] findOrder(int numCourses, int[][] prerequisites) {
+    static int[] topologicalOrderKahnAlgorithm(int numCourses, int[][] prerequisites) {
 
         Map<Integer, List<Integer>> outEdges = new HashMap<>();
         int[] inEdges = new int[numCourses];
@@ -59,5 +52,9 @@ public class CourseScheduleII {
         }
 
         return solutionIndex == numCourses ? solution : new int[]{};
+    }
+
+    public static int[] findOrder(int numCourses, int[][] prerequisites) {
+        return topologicalOrderKahnAlgorithm(numCourses, prerequisites);
     }
 }
