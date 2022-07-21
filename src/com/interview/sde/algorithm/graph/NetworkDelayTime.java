1951302f6f0028764dcb2d8277cf1bfce566fa80
vcamarggo <vinicamarggo@hotmail.com>
2022-07-21 19:18:00 +0100

    Graph problems

---
+++ src/com/interview/sde/algorithm/graph/NetworkDelayTime.java
@@ -0,0 +1,37 @@
+package com.interview.sde.algorithm.graph;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode.com/problems/network-delay-time/
+public class NetworkDelayTime {
+    public int networkDelayTime(int[][] times, int n, int start) {
+        final Map<Integer, Map<Integer, Integer>> adjacencyList = new HashMap<>(n);
+        final int[] costFromStart = new int[n];
+        Arrays.fill(costFromStart, Integer.MAX_VALUE);
+
+        costFromStart[start - 1] = 0;
+
+        for (int[] time : times) {
+            adjacencyList.computeIfAbsent(time[0], k -> new HashMap<>()).put(time[1], time[2]);
+        }
+
+        //BellmanFord
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (Map.Entry<Integer, Integer> edge : adjacencyList.getOrDefault(i, new HashMap<>()).entrySet()) {
+                    int newDistance = costFromStart[i - 1] == Integer.MAX_VALUE ? Integer.MAX_VALUE : costFromStart[i - 1] + edge.getValue();
+                    if (newDistance < costFromStart[edge.getKey() - 1]) {
+                        costFromStart[edge.getKey() - 1] = newDistance;
+                    }
+                }
+
+            }
+        }
+
+        //Get the maximum distance from start node, which is the same as maximum delay
+        int max = Arrays.stream(costFromStart).max().getAsInt();
+        return max == Integer.MAX_VALUE ? -1 : max;
+    }
+}
---
+++ src/com/interview/sde/algorithm/graph/PossibleBipartition.java
@@ -0,0 +1,69 @@
+package com.interview.sde.algorithm.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode.com/problems/possible-bipartition/
+public class PossibleBipartition {
+    public static class DisjointSet {
+        final Map<Integer, Integer> parents;
+        final Map<Integer, Integer> ranks;
+
+        DisjointSet(int n) {
+            this.parents = new HashMap<>();
+            this.ranks = new HashMap<>();
+            for (int i = 1; i <= n; i++) {
+                parents.put(i, i);
+                ranks.put(i, 1);
+            }
+        }
+
+        void union(int a, int b) {
+            int parentA = findParent(a);
+            int parentB = findParent(b);
+            if (parentA != parentB) {
+                if (ranks.get(parentA) > ranks.get(parentB)) {
+                    parents.put(parentB, parentA);
+                    ranks.compute(parentA, (key, value) -> value + ranks.get(parentB));
+                } else {
+                    parents.put(parentA, parentB);
+                    ranks.compute(parentB, (key, value) -> value + ranks.get(parentA));
+                }
+            }
+        }
+
+        int findParent(int i) {
+            while (i != parents.get(i)) {
+                parents.put(i, i = parents.get(i));
+            }
+            return parents.get(i);
+        }
+    }
+
+
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        DisjointSet ds = new DisjointSet(n);
+        Map<Integer, Integer> partitionMapped = new HashMap<>(n);
+
+        for (int[] dislikePair : dislikes) {
+            int a = dislikePair[0];
+            int b = dislikePair[1];
+            if (ds.findParent(a) == ds.findParent(b)) {
+                return false;
+            }
+
+            if (partitionMapped.containsKey(a)) {
+                ds.union(partitionMapped.get(a), b);
+            } else {
+                partitionMapped.put(a, b);
+            }
+
+            if (partitionMapped.containsKey(b)) {
+                ds.union(partitionMapped.get(b), a);
+            } else {
+                partitionMapped.put(b, a);
+            }
+        }
+        return true;
+    }
+}
