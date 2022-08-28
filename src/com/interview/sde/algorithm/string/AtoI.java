b01b5af1f7045a45535388609e23a365a48a86c8
vcamarggo <vinicamarggo@hotmail.com>
2022-08-28 11:08:40 +0100

    A to I

---
+++ src/com/interview/sde/algorithm/string/AtoI.java
@@ -0,0 +1,48 @@
+package com.interview.sde.algorithm.string;
+
+import java.math.BigInteger;
+
+//https://leetcode.com/problems/string-to-integer-atoi/
+public class AtoI {
+    public int myAtoi(String s) {
+        boolean digitStarted = false;
+        boolean digitFinished = false;
+        boolean foundDigit = false;
+
+        StringBuilder digits = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (!digitFinished) {
+                if (!digitStarted && (c == '-' || c == '+')) {
+                    digits.append(c);
+                    digitStarted = true;
+                } else if (Character.isDigit(c)) {
+                    digits.append(c);
+                    digitStarted = true;
+                    foundDigit = true;
+                } else if (digitStarted) {
+                    digitFinished = true;
+                } else if (c != ' ') {
+                    return 0;
+                }
+            }
+        }
+
+        if (digits.length() == 0 || !foundDigit) {
+            return 0;
+        }
+
+        BigInteger digitsValue = new BigInteger(digits.toString());
+
+        if (digitsValue.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) > 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (digitsValue.compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) < 0) {
+            return Integer.MIN_VALUE;
+        }
+
+
+        return digitsValue.intValue();
+    }
+}
