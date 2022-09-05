fc8b176444f1d483410f8752801d9efc0bf07309
vcamarggo <vinicamarggo@hotmail.com>
2022-09-05 18:42:28 +0100

    Easy problems

---
+++ src/com/interview/sde/algorithm/tree/RemoveAllNodeEqualsVal.java
@@ -0,0 +1,41 @@
+package com.interview.sde.algorithm.tree;
+
+//https://leetcode.com/problems/remove-linked-list-elements/
+public class RemoveAllNodeEqualsVal {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = head;
+
+        if (head == null)
+            return null;
+
+        while (head.next != null) {
+            if (head.next.val == val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+
+        if (sentinel.val == val) {
+            return sentinel.next;
+        }
+        return sentinel;
+    }
+}
