0bc05f9406a2b2bbc87296010dc6a4330e5a8126
vcamarggo <vinicamarggo@hotmail.com>
2022-09-06 20:30:27 +0100

    Search problems

---
+++ src/com/interview/sde/algorithm/string/Isomorphic.java
@@ -0,0 +1,27 @@
+package com.interview.sde.algorithm.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode.com/problems/isomorphic-strings/
+public class Isomorphic {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> sToT = new HashMap<>();
+        Map<Character,Character> tToS = new HashMap<>();
+
+        for(int i =0 ; i< s.length();i++){
+            if(!sToT.containsKey(s.charAt(i)) && !tToS.containsKey(t.charAt(i))){
+                sToT.put(s.charAt(i), t.charAt(i));
+                tToS.put(t.charAt(i), s.charAt(i));
+            } else if (sToT.containsKey(s.charAt(i)) && t.charAt(i) != sToT.get(s.charAt(i))
+                    || tToS.containsKey(t.charAt(i)) && s.charAt(i) != tToS.get(t.charAt(i)) ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        new Isomorphic().isIsomorphic("paper", "title");
+    }
+}
