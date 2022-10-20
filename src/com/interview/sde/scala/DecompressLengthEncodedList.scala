5373f8f8430efc2fdae01c1da3fea375ed07c034
vcamarggo <vinicamarggo@hotmail.com>
2022-10-20 23:47:53 +0100

    Recursion vs flatmap scala

--- src/com/interview/sde/scala/DecompressLengthEncodedList.scala
+++ src/com/interview/sde/scala/DecompressLengthEncodedList.scala
@@ -1,10 +1,14 @@
 //https://leetcode.com/problems/decompress-run-length-encoded-list/
 object DecompressLengthEncodedList {
   def decompressRLElist(nums: Array[Int]): Array[Int] = {
+//Option1
+//    if(!nums.isEmpty) Array.fill(nums.head)(nums.tail.head).concat(decompressRLElist(nums.tail.tail)) else nums
+//Option 2
 //    nums match {
 //      case Array(e1, e2, tail@_*) => Array.fill(e1)(e2).concat(decompressRLElist(tail.toArray))
 //      case _ => Array()
 //    }
+    //Option 3
     nums.grouped(2).flatMap(pair => Array.fill(pair(0))(pair(1))).toArray
   }
 
